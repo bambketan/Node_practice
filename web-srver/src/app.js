155982dbe3d180edcbc545a97e031bfc2cf4df72
bambketan <bambketan@gmail.com>
2021-12-09 18:51:55 +0530

    3 files modified

--- web-srver/src/app.js
+++ web-srver/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-  res.send('Hello');
+  res.send('<h1>Used the h1 tag ie html tag</h1>');
 });
 
 app.get('/help', (req, res) => {
